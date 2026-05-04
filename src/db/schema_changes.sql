49f116d7443f6e039c62ebec23003999cf1d36dd
Sneha Ingle <94785951+Sneha-Ingle@users.noreply.github.com>
2026-05-04 22:07:55 +0530

    Update schema_changes.sql

--- src/db/schema_changes.sql
+++ src/db/schema_changes.sql
@@ -11,8 +11,8 @@ quantity int not null
 
 create table users ( 
 id int auto_increment primary key,
-fname varchar(50) not null,
-lname varchar(50) not null,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
 username varchar(50) not null unique,
 password varchar(50) not null ,
 city varchar(50),
@@ -21,4 +21,5 @@ mobile varchar(15) not null unique,
 role ENUM('user','admin') DEFAULT 'user',
 createdAt timestamp default current_timestamp 
 );
+
 select * from products;
